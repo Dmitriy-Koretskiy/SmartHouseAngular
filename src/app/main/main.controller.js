c9bc2d3646d00537066d084eb108a13960d7ca62
Dmitriy-Koretskiy <dmitriy.koretskiy@gmail.com>
2016-02-29 18:54:48 +0200

    Add dashboard

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,40 +6,46 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($http,$timeout, webDevTec, toastr) {
-    var vm = this;
+  function MainController($scope, $http) {
+     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1456177691770;
-    vm.showToastr = showToastr;
-    vm.tests = [];
+    // vm.showToastr = showToastr;
+    vm.tests2 = [];
+    $scope.tests = [];
 
-    $http.get("/api/startpage").success(function (data, status, headers, config) {
-      tests = data.options;
+    $http.get("api/startpage").success(function (result) {
+      $scope.tests = result;
+      vm.tests2= result.data;
 
     });
 
-    activate();
+    // activate();
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    function getRooms(){
+
     }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
+    // function activate() {
+    //   getWebDevTec();
+    //   $timeout(function() {
+    //     vm.classAnimation = 'rubberBand';
+    //   }, 4000);
+    // }
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
+    // function showToastr() {
+    //   toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    //   vm.classAnimation = '';
+    // }
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+    // function getWebDevTec() {
+    //   vm.awesomeThings = webDevTec.getTec();
+
+    //   angular.forEach(vm.awesomeThings, function(awesomeThing) {
+    //     awesomeThing.rank = Math.random();
+    //   });
+    // }
   }
 })();
